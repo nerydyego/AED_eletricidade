11d135d83b72775e2d94e1a9f00e200b83b7c023
nerydyego <dyegonery@gmail.com>
2025-06-21 21:41:45 -0300

    Criação de documento final para apresentação

--- src/data_cleaning/cleaning.py
+++ src/data_cleaning/cleaning.py
@@ -14,5 +14,17 @@ def drop_duplicados(dataframe: pd.DataFrame):
     return dataframe
 
 def limpa_dados_NAN(dataframe: pd.DataFrame):
+    """Remove todas as linhas que contêm valores NaN de um DataFrame.
+
+    Args:
+        dataframe (pd.DataFrame): O DataFrame de entrada que será limpo.
+
+    Returns:
+        pd.DataFrame: O DataFrame com todas as linhas contendo valores NaN removidas.
+
+    Notes:
+        - A remoção é feita inplace, ou seja, o DataFrame original é modificado.
+        - Não há tratamento adicional para outros tipos de valores ausentes (ex.: None, strings vazias).
+    """
     dataframe.dropna(inplace=True)
     return dataframe
